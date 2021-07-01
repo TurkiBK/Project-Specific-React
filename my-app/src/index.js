42d6eea335e0b68994139d2fe519540246b8125c
Turki BK <tbkaily.gmail.com>
2021-06-30 18:20:45 -0700

    w

---
+++ my-app/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class Squer extends React.Component {
+    render() {
+        return (
+            <button className="square"
+                {/*TODO*/}
+            </ button>
+
+                );
+ }
+
+}
+                ReactDOM.render(
+                <Game />,
+                document.getElementById('root')
+                );
